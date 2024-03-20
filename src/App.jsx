2149b4a78c611968ce7b698ff486832404066f98
zadumka <90329284+zadumka@users.noreply.github.com>
2024-03-20 10:55:30 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -34,8 +34,7 @@ function App() {
 
   const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
 
-  const positivePercentage = Math.round((feedback.good / totalFeedback) * 100);
-
+  const postiveFeedback = Math.round((feedback.good / totalFeedback) * 100);
   return (
     <div>
       <Description />
