7774af7523128cc16e780182e5fdacb69620ab6b
zadumka <90329284+zadumka@users.noreply.github.com>
2024-03-20 11:00:18 +0200

    Update Feedback.jsx

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,11 +1,11 @@
-const Feedback = ({ good, neutral, bad, totalFeedback, postiveFeedback }) => {
+const Feedback = ({ good, neutral, bad, totalFeedback, positiveFeedback }) => {
   return (
     <div>
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
       <p>Total: {totalFeedback}</p>
-      <p>postiveFeedback: {postiveFeedback}%</p>
+      <p>positiveFeedback: {positiveFeedback}%</p>
     </div>
   );
 };
