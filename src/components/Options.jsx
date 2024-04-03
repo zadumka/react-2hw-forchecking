029d69c9f798daf4b26d694b45284ab69cdbc061
zadumka <90329284+zadumka@users.noreply.github.com>
2024-04-03 12:16:32 +0300

    Update Options.jsx

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,15 +1,31 @@
+import css from "./Options.module.css";
 
-const Options = ({ updateFeedback, resetFeedback, totalFeedback }) => {
+const Options = ({ updateFeedback, resetFeedback }) => {
   return (
-    <div>
-      <button onClick={() => updateFeedback('good')}>Good</button>
-      <button onClick={() => updateFeedback('neutral')}>Neutral</button>
-      <button onClick={() => updateFeedback('bad')}>Bad</button>
-      {totalFeedback > 0 && (
-        <button onClick={() => resetFeedback()}>Reset</button>
-      )}
-    </div>
-  );
-};
+    <>
+      <ul className={css.bntWrapper}>
+@@ -14,7 +14,11 @@ const Options = ({ updateFeedback, resetFeedback }) => {
+          <button onClick={() => updateFeedback("bad")}>Bad</button>
+        </li>
+        <li>
+          <button onClick={() => resetFeedback()}>Reset</button>
+        </li>
+      </ul>
+    </>
 
-export default Options;
+
+
+// const Options = ({ updateFeedback, resetFeedback, totalFeedback }) => {
+//   return (
+//     <div>
+//       <button onClick={() => updateFeedback('good')}>Good</button>
+//       <button onClick={() => updateFeedback('neutral')}>Neutral</button>
+//       <button onClick={() => updateFeedback('bad')}>Bad</button>
+//       {totalFeedback > 0 && (
+//         <button onClick={() => resetFeedback()}>Reset</button>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Options;
